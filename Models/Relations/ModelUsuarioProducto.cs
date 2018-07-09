a188f778f67c50e6952762ad67fbc8b058d8ab8b
GINPER\David Gomez <dgomez@biosystems.es>
2018-07-09 13:14:46 +0200

    Nuevo Modelo de relación "ModelUsuarioProducto"

---
+++ Models/Relations/ModelUsuarioProducto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+ 
+public class ModelUsuarioProducto : ModelBase
+{
+    public ModelUsuarioProducto() { }
+
+    public int IdUsuario { get; set; }
+    public int IdProdcuto { get; set; }
+
+    public ModelUsuarioAlumno usuario_Alumno { get; set; }
+    public ModelProducto producto { get; set; }
+}

fdf5abb90878887bc14554ee878c89a81d7be057
David Gomez <dgomez@biosystems.es>
2018-07-14 01:09:00 +0200

    Desactivación de algunas alertas de VisualStudio

---
+++ GlobalSuppressions.cs
@@ -0,0 +1,10 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Estilo", "IDE1006:Estilos de nombres", Justification = "<Pendiente>", Scope = "member", Target = "~P:ModelUsuarioAlumno.idusuariogestor")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Estilo", "IDE1006:Estilos de nombres", Justification = "<Pendiente>", Scope = "member", Target = "~P:ModelUsuarioAlumno.idrol")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Estilo", "IDE1006:Estilos de nombres", Justification = "<Pendiente>", Scope = "member", Target = "~P:ModelUsuarioAlumno.rol")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Estilo", "IDE1006:Estilos de nombres", Justification = "<Pendiente>", Scope = "member", Target = "~P:ModelUsuarioAlumno.usuario_gestor")]

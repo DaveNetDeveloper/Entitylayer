1c8681e8417f781df762c7198ea58701a4ba42a6
David Gomez <dgomez@biosystems.es>
2018-08-12 14:48:40 +0200

    nuevo model para Idioma

---
+++ Models/ModelIdioma.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class ModelIdioma : ModelBase, IModel
+{
+    #region [ ctors. ]
+
+    public ModelIdioma() { }
+
+    #endregion
+
+    #region [ public properties ]
+
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public string Cultura { get; set; }
+    public string Codigo { get; set; }
+
+    #endregion
+}
